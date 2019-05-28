48a769d0bf031a35f4193e20f873024cc5ef1b12
Detola <oyawoye.ade@gmail.com>
2019-05-28 16:53:49 +0100

    Added Update and Delete routes and tested all CRUD Event routes in client

--- controllers/events.py
+++ controllers/events.py
@@ -27,3 +27,47 @@ def create():
         return jsonify({'message': 'Validation failed', 'errors': err.messages}), 422
 
     return schema.dumps(event), 201
+
+@router.route('/events/<int:event_id>', methods=['GET'])
+@db_session
+def show(event_id):
+    schema = EventSchema()
+    event = Event.get(id=event_id)
+
+    if not event:
+        abort(404)
+
+
+    return schema.dumps(event)
+
+
+@router.route('/events/<int:event_id>', methods=['PUT'])
+@db_session
+def update(event_id):
+    schema = EventSchema()
+    event = Event.get(id=event_id)
+
+    if not event:
+        abort(404)
+
+    try:
+        data = schema.load(request.get_json())
+        event.set(**data)
+        db.commit()
+    except ValidationError as err:
+        return jsonify({'message': 'Validation failed', 'errors': err.messages}), 422
+
+    return schema.dumps(event)
+
+@router.route('/events/<int:event_id>', methods=['DELETE'])
+@db_session
+def delete(event_id):
+    event = Event.get(id=event_id)
+
+    if not event:
+        abort(404)
+
+    event.delete()
+    db.commit()
+
+    return '', 204
